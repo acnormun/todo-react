843ed00e4046193435e78cb710fd0de8e823132f
Ana Clara <anaclaranoronha.m@gmail.com>
2023-03-30 11:42:02 -0300

    aaa

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Header } from './components/Header'
 import { Tab } from './components/Tab'
+import {Task} from './components/Task'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
     <>
       <Header/>
       <Tab/>
+      <Task/>
     </>
   )
 }
---
+++ src/components/Task.tsx
@@ -0,0 +1,11 @@
+import styles from './Task.module.css'
+
+export function Task(){
+    return(
+        <div className={styles.task}>
+            <input type="radio" />
+            <p>Teste</p>
+        </div>
+
+    )
+}
